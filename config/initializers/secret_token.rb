664a7ad2e0ce9b4c6ccbd683d03245cb4bd93061
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-03-27 19:15:50 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SolarSystem::Application.config.secret_key_base = '58314c3281794c6b68846a4e509e450c828cc203a40402c461f3cc13dd99208f5ed3d73805632fdb1708adc1cf4529b3e83177eb0efcaa74665a9ea8696cdfcc'
