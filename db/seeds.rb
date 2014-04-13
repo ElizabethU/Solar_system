28b14ad57a34ea9e7e3944091126d217a276c5b1
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-04-13 15:27:03 -0700

    modal works for earth

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+bodies = Body.create([{name: "mercury", diameter: .2, color: 'aba9aa', x:, y:, z:, current: true}, {name: "venus", diameter: .4, color: 'e0f7a6', x:, y:, z:, current: true},
+  {name: "earth", diameter: .4, color: '0077ff', x:, y:, z:, current: true},
+  {name: "mars", diameter: .3, color: 'd6965e', x:, y:, z:, current: true},
+  {name: "jupiter", diameter: .6, color: 'faa161', x:, y:, z:, current: true},
+  {name: "saturn", diameter: .5, color: 'd9ba73', x:, y:, z:, current: true},
+  {name: "uranus", diameter: .3, color: 'a7c2bd', x:, y:, z:, current: true},
+  {name: "neptune", diameter: .3, color: '829dd1', x:, y:, z:, current: true}])
