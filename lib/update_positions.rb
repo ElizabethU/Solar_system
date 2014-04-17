215f16c36cfa2bd6f4374a34098d1cc5bd604b73
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-04-16 17:04:28 -0700

    updates service calls to specific year

--- lib/update_positions.rb
+++ lib/update_positions.rb
@@ -10,16 +10,26 @@ class Updater
   def update
     @bodies.each do |body|
       next if body.name == 'sol' || body.current == false
-      this_planet_hash = HTTParty.get("http://ec2-54-187-83-106.us-west-2.compute.amazonaws.com/bodies/#{body.name}").parsed_response
-      if this_planet_hash["body"]["years"][@year] && this_planet_hash["body"]["years"][@year][@day]
+
+      api_response = HTTParty.get("http://ec2-54-187-83-106.us-west-2.compute.amazonaws.com/bodies/#{body.name}/year/#{@year}")
+
+      if api_response.status != 200
+        #no data for this year
+        body.update(current: false) and next
+      else
+        this_planet_hash = api_response.parsed_response
+      end
+      
+      if this_planet_hash[@day]
         puts "Hey, I found the day in the hash!"
-        today = this_planet_hash["body"]["years"][@year][@day]
+        today = this_planet_hash[@day]
         body.x = today["x"]
         body.y = today["y"]
         body.z = today["z"]
       else
         body.current = false
       end
+      
       body.save
       puts "I saved a thing!"
     end
