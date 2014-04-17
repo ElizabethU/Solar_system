8cc5b94aea16a517bb81ccd579362ee6720e86f6
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-04-17 16:02:26 -0700

    added to rake task

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140411231000) do
+ActiveRecord::Schema.define(version: 20140417203825) do
+
+  create_table "articles", force: true do |t|
+    t.text     "title"
+    t.string   "url"
+    t.text     "summary"
+    t.string   "date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "bodies", force: true do |t|
     t.string   "name"
--- lib/news_stories.rb
+++ lib/news_stories.rb
@@ -6,6 +6,17 @@ class NewNews
   def update
     @bodies.each do |body|
       feed = Feedjira::Feed.fetch_and_parse("https://news.google.com/news/feeds?q=#{body.name}nasa&output=rss")
+
+      next if feed.entries.count == 0
+
+      feed.entries.each do |article|
+        Article.create(title: article.title, url: article.url, summary: article.summary, date: article.date)
+
+      end
     end
+  end
+
+  def already_article(title)
+    Article.where(title: title).count > 0
   end
 end
