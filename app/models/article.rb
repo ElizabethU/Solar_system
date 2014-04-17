0976a8f727747b3ea4ce25a87473d817531322aa
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-04-17 14:51:35 -0700

    added validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ActiveRecord::Base
+  validates :title, uniqueness: true
+  validates :title, presence: true
+  validates :url, presence: true
+  validates :summary, presence: true
+  validates :date, presence: true
 end
--- app/models/body.rb
+++ app/models/body.rb
@@ -1,2 +1,4 @@
 class Body < ActiveRecord::Base
+  validates :name, uniqueness: true
+  validates :name, presence: true
 end
