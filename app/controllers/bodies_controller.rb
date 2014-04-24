b34c7e089a548497795a14fd84cb19c6292c4722
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-04-23 17:38:07 -0700

    awful hash not working, everything else promising

--- app/controllers/bodies_controller.rb
+++ app/controllers/bodies_controller.rb
@@ -2,4 +2,15 @@ class BodiesController < ApplicationController
   def index
     @bodies = Body.all
   end
+
+  def hash
+    @hash = {}
+    Body.where(current: true).each do |planet|
+      @hash[planet] = HTTParty.get("http://ec2-54-187-83-106.us-west-2.compute.amazonaws.com/bodies/#{planet.name}/year/2013")
+    end
+    render json: @hash
+  end
 end
+
+# day += 1
+# @hash[planet]['2013'][day.to_s]['x'] 
