9fc1af36022a3d408fe71a5930db474c3074b85a
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-04-26 18:43:04 -0700

    fails gracefully on mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def on_a_phone?
+    request.user_agent.downcase.include?('mobile')
+  end
+
+  helper_method :on_a_phone?
 end
